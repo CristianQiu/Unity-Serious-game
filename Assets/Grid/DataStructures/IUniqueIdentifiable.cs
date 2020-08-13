40dd72e650d6c264ea2add659b385bce43259aaa
Cristian Qiu Félez <CristianQiuFelez@gmail.com>
2020-08-13 17:53:49 +0200

    Deleted stuff that did not belong to this project. Updated the project version and packages, added gif

--- Assets/Grid/DataStructures/IUniqueIdentifiable.cs
+++
@@ -1,8 +0,0 @@
-﻿/// <summary>
-/// Interface to implement an id which should be unique for each instance of an object
-/// </summary>
-/// <typeparam name="T"></typeparam>
-public interface IUniqueIdentifiable<T>
-{
-    int UniqueId { get; }
-}
